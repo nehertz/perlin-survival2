374099eafc3190672b83a7a953fb88758a745d72
nehertz <nathan.hertz@student.nmt.edu>
2018-03-28 01:36:14 -0600

    Added basic support for a weapon and enemies
    Fight evil cubes with a ray gun that is also a rectangular prism jutting from your body.

---
+++ Perlin Survival/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Implements the functionality of an enemy that has health and can die
+ */
+public class Enemy : MonoBehaviour {
+    public float health = 50f;
+
+    /**
+     * Makes the enemy turn red for 0.3 seconds
+     */
+    IEnumerator TurnRed()
+    {
+        float timePassed = 0;
+        Color color = GetComponent<Renderer>().material.color;
+
+        while (timePassed < 0.3) {
+            GetComponent<Renderer>().material.color = new Color(color.r + 0.5f, color.g, color.b);
+
+            timePassed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b);
+    }
+
+    /**
+     * Method that subtracts the damage taken from the total health; supports death
+     */
+    public void TakeDamage(float damage)
+    {
+        StartCoroutine(TurnRed());
+
+        health -= damage;
+
+        if (health <= 0) {
+            Die();
+        }
+    }
+
+    /**
+     * Method that imitates death; deletes the object from the scene
+     */
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Perlin Survival/Assets/Scripts/Weapon/RayGun.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * First weapon added to the game: a ray gun
+ * Inherits from RangedWeapon and has a cool laser effect when you fire
+ */
+public class RayGun : MonoBehaviour {
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera view;
+    public LineRenderer laser;
+    public ParticleSystem beam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+            FireWeapon();
+        }
+	}
+
+    /**
+     * "Fires the weapon" or shoots a raycast out from the weapon and if it hits an enemy, subtracts health from it
+     */
+    public void FireWeapon()
+    {
+        beam.Play();
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit raycastHit;
+
+        laser.enabled = true;
+        laser.SetPosition(0, ray.origin);
+
+        // Raycast hit an object
+        if (Physics.Raycast(view.transform.position, view.transform.forward, out raycastHit, range)) {
+            laser.SetPosition(1, raycastHit.point);
+            Debug.Log(raycastHit.transform.name);
+
+            Enemy target = raycastHit.transform.GetComponent<Enemy>();
+
+            if (target != null) {
+                target.TakeDamage(damage);
+            }
+        } else {
+            laser.SetPosition(1, ray.GetPoint(100));
+        }
+
+        laser.enabled = false;
+    }
+}
