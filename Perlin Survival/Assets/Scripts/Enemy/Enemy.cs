ab84543464bf75b86fe844d22f3c52799abec722
nehertz <nathan.hertz@student.nmt.edu>
2018-04-06 00:34:08 -0600

    Adding the desert preset for the terrain generation and enemy and player health bars
    Adding the desert terrain options I created a little while ago. Includes the three terrain components and can be found in the TerrainAssets/Desert folder.
    
    Also added a player health bar on the screen. The player's health currently ticks down every frame just to show the bar works.
    
    Enemy health bars are in and enemies can actually take damage right now, unlike the player. I also added the enemy name, which I don't know if I'll keep.

--- Perlin Survival/Assets/Scripts/Enemy/Enemy.cs
+++ Perlin Survival/Assets/Scripts/Enemy/Enemy.cs
@@ -1,11 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 /**
  * Implements the functionality of an enemy that has health and can die
  */
-public class Enemy : MonoBehaviour {
-    public float health = 50f;
+public class Enemy : MonoBehaviour
+{
+    public float startHealth = 50f;
+    public float health;
+    public Image healthBar;
+    public Canvas enemyInfo;
+    public Camera playerCam;
+
+    void Start()
+    {
+        health = startHealth;
+    }
+
+    void Update()
+    {
+        //Rect infoRect = enemyInfo.pixelRect;
+        RectTransform infoTransform = enemyInfo.GetComponent<RectTransform>();
+
+        if (CountCornersVisibleFrom(infoTransform, playerCam) > 0) {
+            // Make the enemy's health bar and name face the player's camera
+            Vector3 faceCamera = playerCam.transform.position - enemyInfo.transform.position;
+
+            enemyInfo.enabled = true;
+            faceCamera.x = 0f;
+            faceCamera.z = 0f;
+
+            enemyInfo.transform.LookAt(playerCam.transform.position - faceCamera);
+            enemyInfo.transform.Rotate(0, 180, 0);
+        } else {
+            enemyInfo.enabled = false;
+        }
+    }
+
+    /**
+     * Counts the number of corners of a RectTransform that are visible to a camera
+     * @param rectTransform The rectTransform
+     * @param camera The camera
+     * @return The number of corners visible to the camera
+     */
+    private static int CountCornersVisibleFrom(RectTransform rectTransform, Camera camera)
+    {
+        // Screen space bounds (assumes camera renders across the entire screen)
+        Rect screenBounds = new Rect(0f, 0f, Screen.width, Screen.height);
+        Vector3[] objectCorners = new Vector3[4];
+        rectTransform.GetWorldCorners(objectCorners);
+
+        int visibleCorners = 0;
+        Vector3 tempScreenSpaceCorner;
+        for (var i = 0; i < objectCorners.Length; i++)
+        {
+            // Transform world space position of corner to screen space
+            tempScreenSpaceCorner = camera.WorldToScreenPoint(objectCorners[i]);
+
+            // If the corner is inside the screen
+            if (screenBounds.Contains(tempScreenSpaceCorner))
+            {
+                visibleCorners++;
+            }
+        }
+        return visibleCorners;
+    }
 
     /**
      * Makes the enemy turn red for 0.3 seconds
@@ -34,6 +94,7 @@ public class Enemy : MonoBehaviour {
         StartCoroutine(TurnRed());
 
         health -= damage;
+        healthBar.fillAmount = health / startHealth;
 
         if (health <= 0) {
             Die();
---
+++ Perlin Survival/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public float startHP = 100f;
+    public float hp = 100f;
+    public Image healthBar;
+    public Text hpText;
+
+	// Use this for initialization
+	void Start () {
+		//
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TakeDamage(0.1f);
+	}
+
+    /**
+     * Method that subtracts the damage taken from the total health; supports death
+     */
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+        healthBar.fillAmount = hp / startHP;
+
+        int hpInt = (int)hp;
+
+        hpText.text = hpInt.ToString();
+
+        if (hp <= 0) {
+            Die();
+        }
+    }
+
+    /**
+     * Method that imitates death; deletes the object from the scene
+     */
+    void Die()
+    {
+        Debug.Log("You died.");
+        // TODO: Implement death
+    }
+}
